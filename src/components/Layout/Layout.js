8e4eb806015039aea0c4e59d3ba78119ab14f70f
colbyfayock <fayock@gmail.com>
2022-04-06 11:26:09 -0300

    init

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 
-import Header from '@components/Header';
 import Footer from '@components/Footer';
 
 import styles from './Layout.module.scss';
@@ -17,7 +16,6 @@ const Layout = ({ children, className, ...rest }) => {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
       <main className={styles.main}>{ children }</main>
       <Footer />
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,63 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
 
 import Layout from '@components/Layout';
 import Container from '@components/Container';
 import Button from '@components/Button';
 
-import images from '@data/images';
-
 import styles from '@styles/Home.module.scss'
 
 export default function Home() {
+  const [action, setAction] = useState();
+
+  function handleOnClick(e) {
+    setAction('click')
+  }
+
   return (
     <Layout>
       <Head>
-        <title>My Images</title>
-        <meta name="description" content="All of my cool images." />
+        <title>Click or Press</title>
+        <meta name="description" content="Click or press the button!" />
       </Head>
 
-      <Container>
-        <h1 className="sr-only">My Images</h1>
+      <Container className={styles.homeContainer}>
+        <h1>Click or Press!</h1>
 
-        <h2 className={styles.header}>Images</h2>
+        <p>
+          <Button onClick={handleOnClick}>
+            Click Me
+          </Button>
+          <Button data-color="gray" onClick={() => setAction(undefined)}>
+            Reset
+          </Button>
+        </p>
 
-        <ul className={styles.images}>
-          {images.map(image => {
-            return (
-              <li key={image.id}>
-                <a href={image.link} rel="noreferrer">
-                  <div className={styles.imageImage}>
-                    <Image width={image.width} height={image.height} src={image.image} alt="" />
-                  </div>
-                  <h3 className={styles.imageTitle}>
-                    { image.title }
-                  </h3>
-                </a>
-              </li>
-            )
-          })}
-        </ul>
+        <h2>What you did to the button...</h2>
+
+        <p className={styles.clickAction}>
+          {!action && (
+            <>
+              <strong>Nothing</strong>
+              <img width="356" src="/images/fry-not-sure.jpg" />
+            </>
+          )}
+
+          {action === 'click' && (
+            <>
+              <strong>Click</strong>
+              <img width="356" src="/images/fry-celebrating.gif" />
+            </>
+          )}
+
+          {action === 'longpress' && (
+            <>
+              <strong>Long Press</strong>
+              <img width="356" src="/images/fry-dancing.gif" />
+            </>
+          )}
+        </p>
+
       </Container>
     </Layout>
   )
